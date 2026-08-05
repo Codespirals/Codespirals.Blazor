4df0ad870a28ec3ff96a002479e219e9ec44573f
Elias-Styner <elias.styner@gmail.com>
2026-08-05 21:58:13 +0200

    building a template from which to create new blazor wasm websites more easily

---
+++ Codespirals.Blazor.WebsiteTemplate/Models/PostMetadata.cs
@@ -0,0 +1,20 @@
+﻿using Codespirals.Base.Extensions;
+
+namespace Codespirals.Blazor.WebsiteTemplate;
+
+public class PostMetadata
+{
+    public string Id => $"{Title.MakeUrlSafe('_').Trim('_').ToLowerInvariant()}";
+    public string? Group { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string[] Tags { get; set; } = [];
+    public DateTime Created { get; set; } = DateTime.UtcNow;
+    public DateTime? Edited { get; set; }
+    public string? ContentUrl { get; set; }
+    public bool IsActive { get; set; } = true;
+    public bool ShowInList { get; set; } = true;
+    public string StickerEmoji { get; set; } = "";
+    public int TechnicalDifficulty { get; set; }
+    public int? EstimatedReadTimeInMinutes { get; set; }
+}
