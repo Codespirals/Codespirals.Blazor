30d32ac639c34a4e039c2db398c3d52cc17c03c7
Elias-Styner <elias.styner@gmail.com>
2026-08-06 17:45:24 +0200

    finished blazor wasm template

---
+++ Codespirals.Blazor.WebsiteTemplate/CodeResources/Constants.cs
@@ -0,0 +1,6 @@
+﻿namespace Codespirals.Blazor.WebsiteTemplate.CodeResources;
+
+public static class Constants
+{
+    public const string SiteName = "Template";
+}
--- Codespirals.Blazor.WebsiteTemplate/wwwroot/posts/HelloWorld.md
+++ Codespirals.Blazor.WebsiteTemplate/wwwroot/posts/HelloWorld.md
@@ -1,4 +1,4 @@
-### Hello World
+### Template Post
 
 Written with markdown formatting and saved as an .md file.
 
